54f95f18062de19504ead3fab60d09902a67e394
Av Pinzur <av@avpinzur.com>
2016-11-17 19:31:38 -0600

    Updated requirements.

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\code\HACKFO~1\2016-P~1\azure\env"
+set "VIRTUAL_ENV=C:\code\HACKFO~1\PROJEC~1\JUNIOR~1\env"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
